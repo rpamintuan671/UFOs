90fb52506fba1723f9256d0d532eed3b608f74fa
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-09-07 13:23:35 -0700

    Final Table Created

--- static/js/app.js
+++ static/js/app.js
@@ -24,6 +24,26 @@ function buildTable(data) {
     });
   }
 
+  function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
+        let cell = row.append("td");
+        cell.text(val);
+        }
+      );
+    });
+  }
+  
   function handleClick() {
     // Grab the datetime value from the filter
     let date = d3.select("#datetime").property("value");
@@ -49,23 +69,6 @@ function buildTable(data) {
   // Build the table when the page loads
   buildTable(tableData);
 
-  
-  function buildTable(data) {
-    // First, clear out any existing data
-    tbody.html("");
-  
-    // Next, loop through each object in the data
-    // and append a row and cells for each value in the row
-    data.forEach((dataRow) => {
-      // Append a row to the table body
-      let row = tbody.append("tr");
-  
-      // Loop through each field in the dataRow and add
-      // each value as a table cell (td)
-      Object.values(dataRow).forEach((val) => {
-        let cell = row.append("td");
-        cell.text(val);
-        }
-      );
-    });
-  }
+
+
+
